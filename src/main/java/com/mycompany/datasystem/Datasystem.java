116b5f9daed717c432c3272bf254260b75b92140
camilo <camiloagudelo090@gmail.com>
2023-03-02 22:46:21 -0500

    diseño de login

---
+++ src/main/java/com/mycompany/datasystem/Datasystem.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datasystem;
+
+import ventanas.Login;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Datasystem {
+
+    public static void main(String[] args) {
+       
+         //esto es para motrar el formulario visual de login
+        Login ventana= new Login();
+       ventana.show();
+
+    }
+}
